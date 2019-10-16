778636847f2acd47473b78291083d49bfb752b8d
124c4a11 <124c4a11@mail.ru>
2019-10-16 07:06:21 +0300

    Router settings

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const path = require('path');
 
 const app = express();
 
+const todoRoutes = require('./routes/todo');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.sendFile('/index.html');
   next();
 });
+
+
+app.use('/api/todo', todoRoutes);
 
 
 module.exports = app;
